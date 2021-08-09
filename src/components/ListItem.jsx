ce4d35633f90dbfd5caa1f8801647871a262a50c
Julian Vicente <julian.vicente97s@gmail.com>
2021-08-09 20:23:21 -0300

    finalizado

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -19,6 +19,7 @@ function ListItem({ title, editItem, removeItem, id }) {
       <h3 className="text-gray-900 dark:text-dark-gray transition duration-500 text-sm capitalize font-semibold">
         {title}
       </h3>
+      
       <ButtonsList
         isHidden={isHidden}
         editItem={editItem}
